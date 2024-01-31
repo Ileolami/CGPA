5efd07760e6087a3ab61deaf9040147e226820f5
Moni-Yahwehson <moniyahwehson@gmail.com>
2024-01-31 11:23:16 +0100

    Worked on the font

--- src/Features/EditButton.jsx
+++ src/Features/EditButton.jsx
@@ -1,20 +1,27 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
 import CgpaContext from "../contexts/CgpaContext";
 import Button from "../component/Button";
-import { useContext } from "react";
+import {  useContext } from "react";
+import { FaPencil } from "react-icons/fa6";
 
 const EditButton = ({ index, onEdit }) => {
-    const { submittedValues, setFormValues, setEditingIndex } = useContext(CgpaContext);
-    const handleEdit = (index) => {
-        setFormValues(submittedValues[index]);
-        setEditingIndex(index);
-    };
-    return (
-        <Button
-            props='edit'
-            onClick={() => handleEdit(index)}
-            className="text-xs px-1 py-1 w-10 h-5 bg-yellow-500 text-white rounded-sm border-none font-medium hover:bg-black hover:text-white"
-        />
-    );
-}
+  const { submittedValues, setFormValues, setEditingIndex } =
+    useContext(CgpaContext);
+  const handleEdit = (index) => {
+    setFormValues(submittedValues[index]);
+    setEditingIndex(index);
+  };
+  return (
+    <Button
+      props="Edit"
+      icon={<FaPencil className="mx-2"/>}
+      onClick={() => handleEdit(index)}
+      className="text-xs px-2 py-2 w-20 h-5 bg-yellow-500 text-white rounded-sm border-none font-medium hover:bg-black hover:text-white"
+    >
+      Edit
+    </Button>
+  );
+};
 
 export default EditButton;
